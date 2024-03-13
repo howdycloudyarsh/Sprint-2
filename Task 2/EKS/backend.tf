741c0c7395bf2c02d094cc0b633095280afd13ea
arshad <ahmad.arshad2009@gmail.com>
2024-03-13 13:46:07 +0530

    modifying some files

--- Task 2/EKS/backend.tf
+++ Task 2/EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks-01"
+    bucket = "cicd-terraform-eks-001"
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
